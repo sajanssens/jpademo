e1997bb9bf1c0064731e082e933c9e937a75cbab
Bram Janssens <bram.janssens@infosupport.com>
2022-04-21 11:12:42 +0200

    Added generic Dao.

--- jpa-with-jse-advanced/src/main/java/com/example/domain/AbstractEntity.java
+++ jpa-with-jse-advanced/src/main/java/com/example/domain/AbstractEntity.java
@@ -1,5 +1,7 @@
 package com.example.domain;
 
+import com.example.dao.Identifiable;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
@@ -7,9 +9,9 @@ import javax.persistence.MappedSuperclass;
 import static javax.persistence.GenerationType.IDENTITY;
 
 @MappedSuperclass
-public abstract class AbstractEntity {
+public abstract class AbstractEntity implements Identifiable<Long> {
     @Id @GeneratedValue(strategy = IDENTITY)
     protected long id;
 
-    public long getId() { return id; }
+    public Long getId() { return id; }
 }
--- jpa-with-jse-basic/src/test/java/com/example/domain/ContactDaoTest.java
+++ jpa-with-jse-basic/src/test/java/com/example/domain/ContactDaoTest.java
@@ -13,8 +13,17 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ContactDaoTest {
